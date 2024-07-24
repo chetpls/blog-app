5e0fc4dcb85537c9d0490a461b33920c96de920c
chetpls <80933465+chetpls@users.noreply.github.com>
2024-07-23 21:34:28 -0700

    Update Register.jsx

--- frontend/blog-app/src/pages/Register.jsx
+++ frontend/blog-app/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../styles/Login.css";
+import "../styles/Login.css"
 
 function Register() {
   const [username, setUsername] = useState("");
